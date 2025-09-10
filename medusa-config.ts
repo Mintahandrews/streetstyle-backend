d22a08cf0e4cb2e93ae210e3404943735b5543ff
Codemintah <86488838+Mintahandrews@users.noreply.github.com>
2025-09-10 03:22:18 +0000

    Disable admin in config and revert build script

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,8 +11,10 @@ module.exports = defineConfig({
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
-  plugins: []
+  plugins: [],
+  admin: {
+    disable: true
+  }
 })
