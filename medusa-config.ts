7fe30c7e18460a6058ea11798ef1e90141d35eea
Codemintah <86488838+Mintahandrews@users.noreply.github.com>
2025-09-10 03:57:16 +0000

    Fix import path

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,4 +1,4 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from '@medusajs/medusa/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
@@ -7,8 +7,8 @@ module.exports = defineConfig({
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
+      storeCors: process.env.STORE_CORS || "https://streetstyle-backend-production.up.railway.app,https://ab.reasonlabsapi.com",
+      adminCors: process.env.ADMIN_CORS || "https://streetstyle-backend-production.up.railway.app,https://ab.reasonlabsapi.com",
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
     },
